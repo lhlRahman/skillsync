6b3248e0c1b6a75cc2fd82e2ee3fe1b63fe87d88
Abdul <lhlrahman@gmail.com>
2024-02-25 04:56:17 -0500

    fixed pick from intergrated

--- lib/db/insert-user-query.jsx
+++ lib/db/insert-user-query.jsx
@@ -2,15 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function createUser(user) {
   const prisma = new PrismaClient();
-  console.log(user);
-  //this is where null occurs
+  user = user.user;
   try {
     const newUser = await prisma.user.create({
       data: {
         email: user.email,
         username: user.username,
-        firstName: user.firstname,
-        lastName: user.lastname,
+        firstName: user.firstName,
+        lastName: user.lastName,
         type: user.type ?? 1,
         clerkId: user.clerkId,
         createdAt: new Date(),
