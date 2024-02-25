2f12967f91dd44bcda840c462297a98df614c547
vincentostrovski <92182422+vincentostrovski@users.noreply.github.com>
2024-02-25 04:42:04 -0500

    Refactor user creation code to use async/await and handle default user type

--- lib/db/insert-user-query.jsx
+++ lib/db/insert-user-query.jsx
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function createUser(user) {
   const prisma = new PrismaClient();
+  console.log(user);
+  //this is where null occurs
   try {
     const newUser = await prisma.user.create({
       data: {
@@ -9,7 +11,7 @@ export default async function createUser(user) {
         username: user.username,
         firstName: user.firstname,
         lastName: user.lastname,
-        type: user.type,
+        type: user.type ?? 1,
         clerkId: user.clerkId,
         createdAt: new Date(),
         updatedAt: new Date(),
