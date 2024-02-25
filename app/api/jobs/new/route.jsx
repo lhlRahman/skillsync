719fdbce4fec5e2d214b174ee416eb9f019ee52a
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-02-25 06:35:53 -0500

    Fix API routes and update job creation

--- app/api/jobs/new/route.jsx
+++ app/api/jobs/new/route.jsx
@@ -1,23 +1,29 @@
 // eampel
 
-import createJob from '@/lib/db/insert-job-query';
-import { NextResponse } from 'next/server';
+import createJob from "@/lib/db/insert-job-query";
+import { NextResponse } from "next/server";
 
 // POST /api/days/new
 // Required fields in body: { Day object } - I think?
 export async function POST(req) {
-    if (req.method === "POST") {
-        console.log(req)
-        const { job, posterId } = await req.json();
-        console.log(job)
-        try {
-            const newJob = await createJob(job, posterId);
+  if (req.method === "POST") {
+    const { job, posterId } = await req.json();
+    console.log(job, posterId);
 
-            return NextResponse.json({ status: 201, data: newJob });
-        } catch (error) {
-            return NextResponse.json({ status: 500 }, { message: `${error}: Internal server error` });
-        }
-    } else {
-        return NextResponse.json({ status: 405 }, { message: "Method not allowed" });
+    try {
+      const newJob = await createJob(job, posterId);
+
+      return NextResponse.json({ status: 201, data: newJob });
+    } catch (error) {
+      return NextResponse.json(
+        { status: 500 },
+        { message: `${error}: Internal server error` }
+      );
     }
+  } else {
+    return NextResponse.json(
+      { status: 405 },
+      { message: "Method not allowed" }
+    );
+  }
 }
