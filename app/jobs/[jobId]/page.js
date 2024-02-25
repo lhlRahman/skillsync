10b03967748948900c3c3a8c5cc7c5a2b50ee0c2
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-02-24 21:46:45 -0500

    Update user authentication logic to use bcrypt hashing algorithm

--- app/jobs/[jobId]/page.js
+++ app/jobs/[jobId]/page.js
@@ -1,7 +1,6 @@
 import Job from "@/components/ui/Job";
 import styles from "../../../styles/EachJob.module.scss";
 export default function EachJob() {
-  // TODO: redirect if the user is not a poster
   return (
     <main id={styles.eachJob}>
       <Job />
