506ec5cb07d9878b702bb7cab2fba33b03f0a70e
Abdul <lhlrahman@gmail.com>
2024-02-25 03:36:42 -0500

    MAKE IT CAPITIAL

--- app/jobs/page.js
+++ app/jobs/page.js
@@ -53,7 +53,7 @@ const items = [
   },
 ];
 
-export default function jobs() {
+export default function Jobs() {
   const [originalItems, setOriginalItems] = useState([]);
   const [filteredItems, setFilteredItems] = useState([]);
   const [filters, setFilters] = useState([]);
