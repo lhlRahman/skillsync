abb9d8bed526d5f4576dce5355c620054a5847b1
Abdul <lhlrahman@gmail.com>
2024-02-25 02:15:44 -0500

    delete application when reject is pressed

---
+++ app/application/delete/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import deleteApplication from '@/lib/db/delete-application';
+
+// POST /api/days/new
+// Required fields in body: { Day object } - I think?
+export async function POST(req) {
+  if (req.method === "POST") {
+    try {
+        const {id} = await req.json();
+        const deleted = await deleteApplication(id);
+
+      return NextResponse.json({ status: 201, data: deleted });
+    } catch (error) {
+      return NextResponse.json({ status: 500 }, { message: `${error}: Internal server error`});
+    }
+  } else {
+    return NextResponse.json({ status: 405 }, { message: "Method not allowed" });
+  }
+}
