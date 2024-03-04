672df58dc8ec360b780a71e3728281286bce1fc5
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-03-03 19:57:14 -0500

    Refactor variable names for clarity

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   headers: () => [
+//     {
+//       source: "/:path*",
+//       headers: [
+//         {
+//           key: "Cache-Control",
+//           value: "no-store",
+//         },
+//       ],
+//     },
+//   ],
+//   images: {
+//     domains: ["vastphotos.com"],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "vastphotos.com/",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+export default {
   headers: () => [
     {
       source: "/:path*",
@@ -11,15 +36,4 @@ const nextConfig = {
       ],
     },
   ],
-  images: {
-    domains: ["vastphotos.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "vastphotos.com/",
-      },
-    ],
-  },
 };
-
-export default nextConfig;
