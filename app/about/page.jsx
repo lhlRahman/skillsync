84861f03dfbc98415f7b1f2c5a5bfb6e66173348
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-03-03 19:03:20 -0500

    Refactor about and contact pages

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,10 @@ import Link from "next/link";
 
 export default function Contact() {
   return (
-    <div className="h-[100vh] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+    <div
+      style={{ paddingTop: "10rem" }}
+      className="w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center"
+    >
       <div className="absolute pointer-events-none inset-0 dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="flex flex-col items-center">
         {/* Header Section with GitHub Logo */}
@@ -11,15 +14,32 @@ export default function Contact() {
           <p className="text-4xl sm:text-7xl text-center font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
             About SkillSync
           </p>
-          <a href="https://github.com/lhlrahman/skillsync" target="_blank"><FaGithub className="w-24 h-24 inline-block ml-4" /></a>
+          <a href="https://github.com/lhlrahman/skillsync" target="_blank">
+            <FaGithub className="w-24 h-24 inline-block ml-4" />
+          </a>
         </div>
         {/* Concise About Section */}
         <div className="text-base sm:text-lg text-justify max-w-4xl mx-auto p-8 bg-white dark:bg-black rounded-lg shadow-md">
-          <p className="pb-5"><strong>NSBE Hacks 2024 Achievements:</strong> Winner of the Black Founders and GoDaddy Challenges; Top 5 in Cohere AI Challenge. Developed to simplify volunteer matching, SkillSync leverages AI to connect volunteers with opportunities that fit their skills and interests.</p>
-          
-          <p className="pb-5"><strong>Technology Stack:</strong> NextJs, React, PostgreSQL, Prisma ORM, TailwindCSS, Scss, Framer Motion, Clerk, and Cohere AI. Focused on intuitive design and semantic search for personalized matches.</p>
-          
-          <p className="pb-5"><strong>Challenges & Successes:</strong> Integrating Cohere AI and creating an intuitive platform were our main challenges. We're proud of developing a functional platform that encourages skill development and community growth within 24 hours.</p>
+          <p className="pb-5">
+            <strong>NSBE Hacks 2024 Achievements:</strong> Winner of the Black
+            Founders and GoDaddy Challenges; Top 5 in Cohere AI Challenge.
+            Developed to simplify volunteer matching, SkillSync leverages AI to
+            connect volunteers with opportunities that fit their skills and
+            interests.
+          </p>
+
+          <p className="pb-5">
+            <strong>Technology Stack:</strong> NextJs, React, PostgreSQL, Prisma
+            ORM, TailwindCSS, Scss, Framer Motion, Clerk, and Cohere AI. Focused
+            on intuitive design and semantic search for personalized matches.
+          </p>
+
+          <p className="pb-5">
+            <strong>Challenges & Successes:</strong> Integrating Cohere AI and
+            creating an intuitive platform were our main challenges. We're proud
+            of developing a functional platform that encourages skill
+            development and community growth within 24 hours.
+          </p>
         </div>
       </div>
     </div>
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,43 +2,46 @@ import { HoverEffect } from "../../components/ui/ContactCard";
 
 export default function Contact() {
   return (
-    <div className="h-[100vh] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+    <div
+      style={{ paddingTop: "6rem" }}
+      className="w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center"
+    >
       <div className="absolute pointer-events-none inset-0 dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className=" flex flex-col">
-      <p className="flex flex-col text-4xl sm:text-7xl text-center font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
-        Contact Us
-      </p>
-      <div className="max-w-5xl mx-auto px-8">
-      <HoverEffect items={projects} />
-    </div>
-    </div>
+        <p className="flex flex-col text-4xl sm:text-7xl text-center font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          Contact Us
+        </p>
+        <div className="max-w-5xl mx-auto px-8">
+          <HoverEffect items={projects} />
+        </div>
       </div>
+    </div>
   );
 }
 
 export const projects = [
-    {
-      title: "Habib Rahman",
-      description:
-        "During the hackathon, I was responsible for designing and implementing the database architecture, as well as contributing to both the frontend and backend development.",
-      link: "https://habibrahman.xyz",
-    },
-    {
-      title: "Vincent Ostrowski",
-      description:
-        "As the Scrum Master, I ensured project efficiency and timely delivery. I also spearheaded the integration of Cohere AI, as well as contributing to the Database Fetching and APIs.",
-      link: "https://github.com/vincentostrowski",
-    },
-    {
-      title: "Arshia Zakeri",
-      description:
-        "I took charge of seamlessly integrating front-end and backend systems, incorporating Semantic search functionality, implementing robust authentication mechanisms, and optimizing overall system performance.",
-      link: "https://github.com/ArshiaZr",
-    },
-    {
-      title: "Raj Zatsarenko",
-      description:
-        "I focused on database fetching and developing the backend API, ensuring seamless data integration and robust server-side functionality.",
-      link: "https://github.com/penszzip",
-    },
-  ];
+  {
+    title: "Habib Rahman",
+    description:
+      "During the hackathon, I was responsible for designing and implementing the database architecture, as well as contributing to both the frontend and backend development.",
+    link: "https://habibrahman.xyz",
+  },
+  {
+    title: "Vincent Ostrowski",
+    description:
+      "As the Scrum Master, I ensured project efficiency and timely delivery. I also spearheaded the integration of Cohere AI, as well as contributing to the Database Fetching and APIs.",
+    link: "https://github.com/vincentostrowski",
+  },
+  {
+    title: "Arshia Zakeri",
+    description:
+      "I took charge of seamlessly integrating front-end and backend systems, incorporating Semantic search functionality, implementing robust authentication mechanisms, and optimizing overall system performance.",
+    link: "https://github.com/ArshiaZr",
+  },
+  {
+    title: "Raj Zatsarenko",
+    description:
+      "I focused on database fetching and developing the backend API, ensuring seamless data integration and robust server-side functionality.",
+    link: "https://github.com/penszzip",
+  },
+];
